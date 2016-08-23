242deb4ba96ce9546a5137c5545790bd925e30af
Dhirendra Singh <dhi.singh@gmail.com>
2016-08-23 14:10:11 +1000

    Added test for Jill agents with durative environment actions

---
+++ integrations/abm-jill/src/test/java/io/github/agentsoz/abmjill/JillModelTest.java
@@ -0,0 +1,195 @@
+package io.github.agentsoz.abmjill;
+
+/*
+ * #%L
+ * BDI-ABM Integration Package
+ * %%
+ * Copyright (C) 2014 - 2016 by its authors. See AUTHORS file.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.qos.logback.classic.Level;
+import io.github.agentsoz.bdiabm.ABMServerInterface;
+import io.github.agentsoz.bdiabm.data.ActionContainer;
+import io.github.agentsoz.bdiabm.data.ActionContent;
+import io.github.agentsoz.bdiabm.data.AgentDataContainer;
+import io.github.agentsoz.bdiabm.data.AgentState;
+import io.github.agentsoz.bdiabm.data.AgentStateList;
+import io.github.agentsoz.bdiabm.data.ActionContent.State;
+import io.github.agentsoz.jill.util.Log;
+
+public class JillModelTest {
+
+    private ByteArrayOutputStream out;
+    private ByteArrayOutputStream err;
+
+	private TestModel jillmodel;
+	private AgentDataContainer adc;
+	private AgentStateList asl;
+	private StubABM abmstub;
+
+	@Before
+	public void setUp() throws Exception {	
+		// Set up I/O
+        out = new ByteArrayOutputStream();
+        err = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        System.setErr(new PrintStream(err));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		jillmodel.finish();
+	}
+
+	@Test
+	public void testDurativeActions() {
+		// number of agents
+		final int nAgents = 3;
+		// number of steps per action
+		final int durationPerAction = 4;
+		// Output printed by TestAgent.updateAction(...) once per step
+		final String[] output = { 
+				"0:TESTACTION:PASSED",
+				"0:TESTACTION:RUNNING",
+				"0:TESTACTION:RUNNING",
+				"0:TESTACTION:RUNNING",
+				"0:TESTACTION:RUNNING",
+				"1:TESTACTION:PASSED",
+				"1:TESTACTION:RUNNING",
+				"1:TESTACTION:RUNNING",
+				"1:TESTACTION:RUNNING",
+				"1:TESTACTION:RUNNING",
+				"2:TESTACTION:PASSED",
+				"2:TESTACTION:RUNNING",
+				"2:TESTACTION:RUNNING",
+				"2:TESTACTION:RUNNING",
+				"2:TESTACTION:RUNNING",
+		};
+		
+		// initialise the BDI and ABM system
+		init(nAgents, durationPerAction);
+		
+		// Run for at least durationPerAction steps plus 2 to make sure 
+		// we are definitely finished
+		for (int i = 0; i < durationPerAction+2; i++) {
+			jillmodel.takeControl(adc);
+			abmstub.takeControl(adc);
+		}
+		
+		// Check the output
+		String[] list = out.toString().split(",");
+		Arrays.sort(list);
+		assertTrue(list.length == output.length);
+		for (int i = 0; i < output.length; i++) {
+			assertTrue(list[i].equals(output[i]));
+		}
+	}
+
+	private void init(int nAgents, int durationPerAction) {
+		// Initialise the data containers and add data for the agents
+		adc = new AgentDataContainer();
+		asl = new AgentStateList();
+		for (int i = 0; i < nAgents; i++) {
+			String agentID = String.format("%d", i);
+			asl.add(new AgentState(agentID));
+			adc.getOrCreate(agentID);
+		}
+
+		// Initialise the jillmodel
+		String[] args = {
+				"--config",
+				"{agents:[{classname:io.github.agentsoz.abmjill.testagent.TestAgent, args:null, count:"+nAgents+"}]," +
+				"logLevel: ERROR," +
+				"logFile: " + JillModelTest.class.getSimpleName() + ".log" +
+				"}"
+				};
+		jillmodel = new TestModel();
+		abmstub = new StubABM(durationPerAction);
+		jillmodel.init(adc, asl, abmstub, args);
+		// Now start the BDI (Jill) system
+		jillmodel.start();		
+	}
+	
+	private class TestModel extends JillModel {
+		
+	}
+	
+	private class StubABM implements ABMServerInterface {
+
+		// All actions will take this many steps to complete
+		private int durationPerAction;
+		
+		// A map of agent:action -> count to keep track of how long
+		// an action has been running
+		private HashMap<String, Integer> agentsDurativeCount;
+		
+		public StubABM(int durationPerAction) {
+			this.durationPerAction = durationPerAction;
+			agentsDurativeCount = new HashMap<String, Integer>();
+		}
+		
+		@Override
+		public void takeControl(AgentDataContainer adc) {
+			Log.trace("Stub ABM Received " + adc);
+			for (String agentId : adc.keySet()) {
+				ActionContainer ac = adc.getOrCreate(agentId).getActionContainer();
+				ActionContent acc = ac.get("TESTACTION");
+				if (acc != null) {
+					// The unique id for this agent:action
+					String mapid = agentId+acc.getAction_type();
+					// If just initiated then add to the map
+					if (acc.getState() == State.INITIATED) {
+						acc.setState(State.RUNNING);
+						agentsDurativeCount.put(mapid, durationPerAction);
+					}
+					// If running then 
+					if (acc.getState() == State.RUNNING) {
+						// Count down
+						int count = agentsDurativeCount.get(mapid);
+						count--;
+						agentsDurativeCount.put(mapid, count);
+						// If done
+						if (count < 0) {
+							acc.setState(State.PASSED);
+						}
+					}
+				}
+			}
+		}
+
+		@Override
+		public Object queryPercept(String agentID, String perceptID) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+}
---
+++ integrations/abm-jill/src/test/java/io/github/agentsoz/abmjill/testagent/TestAgent.java
@@ -0,0 +1,89 @@
+package io.github.agentsoz.abmjill.testagent;
+
+import io.github.agentsoz.bdiabm.data.ActionContent;
+import io.github.agentsoz.bdiabm.data.ActionContent.State;
+
+/*
+ * #%L
+ * Jill Cognitive Agents Platform
+ * %%
+ * Copyright (C) 2014 - 2016 by its authors. See AUTHORS file.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import io.github.agentsoz.jill.lang.Agent;
+import io.github.agentsoz.jill.lang.AgentInfo;
+import io.github.agentsoz.jill.util.Log;
+
+import java.io.PrintStream;
+
+@AgentInfo(hasGoals={"io.github.agentsoz.abmjill.testagent.GoalA", "io.github.agentsoz.abmjill.genact.EnvironmentAction"})
+public class TestAgent extends Agent implements io.github.agentsoz.bdiabm.Agent {
+
+	private PrintStream writer = null;
+
+	public TestAgent(String str) {
+		super(str);
+	}
+
+	@Override
+	public void start(PrintStream writer, String[] params) {
+		this.writer = writer;
+		post(new GoalA("gA"));
+	}
+	
+	@Override
+	public void finish() {
+	}
+
+	@Override
+	public void init(String[] args) {
+		Log.warn("TestAgent using a stub for io.github.agentsoz.bdiabm.Agent.init(...)");
+	}
+
+	@Override
+	public void start() {
+		Log.warn("TestAgent using a stub for io.github.agentsoz.bdiabm.Agent.start()");
+	}
+
+	@Override
+	public void handlePercept(String perceptID, Object parameters) {
+		Log.warn("TestAgent using a stub for io.github.agentsoz.bdiabm.Agent.handlePercept(...)");
+	}
+
+	@Override
+	public void packageAction(String actionID, Object[] parameters) {
+		Log.warn("TestAgent using a stub for io.github.agentsoz.bdiabm.Agent.packageAction(...)");
+	}
+
+	@Override
+	public void updateAction(String actionID, ActionContent content) {
+		Log.info("Agent"+getId()+" received action update: "+content);
+		if (content.getAction_type().equals("TESTACTION")) {
+			writer.print(getId()+":"+content.getAction_type()+":"+content.getState()+",");
+			if (content.getState()==State.PASSED) {
+				// Wake up the agent that was waiting for external action to finish
+				suspend(false);
+			}
+		}
+	}
+
+	@Override
+	public void kill() {
+		// TODO Auto-generated method stub
+	}
+}
