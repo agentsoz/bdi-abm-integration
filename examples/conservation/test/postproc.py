fb46e2c5c57e687cb5f9d3446e477e08bae33804
Dhirendra Singh <dhi.singh@gmail.com>
2017-06-21 09:20:29 +1000

    conservation: fixed postproc to correctly process gzipped/other outputs

--- examples/conservation/test/postproc.py
+++ examples/conservation/test/postproc.py
@@ -25,7 +25,7 @@ execfile(args.EXPERIMENT_DIR + '/config')
 
 
 ## output variable names
-filenames = ["auction_statistics_001.csv", "agents_ce_001.csv", "agents_pm_001.csv", "bids_001.csv", "config_parameters.csv", "number_of_bids_per_agent001.csv", "number_of_successful_bids_per_agent001.csv", "successful_bid_price_per_agent001.csv", "successful_opportunity_cost_per_agent001.csv", "total_bid_price_per_agent001.csv", "total_opportunity_cost_per_agent001.csv"]
+filenames = ["auction_statistics_001.csv.gz", "agents_ce_001.csv.gz", "agents_pm_001.csv.gz", "bids_001.csv.gz", "config_parameters.csv", "number_of_bids_per_agent001.csv.gz", "number_of_successful_bids_per_agent001.csv.gz", "successful_bid_price_per_agent001.csv.gz", "successful_opportunity_cost_per_agent001.csv.gz", "total_bid_price_per_agent001.csv.gz", "total_opportunity_cost_per_agent001.csv.gz"]
 tablenames = ["auction_statistics", "agents_ce", "agents_pm", "bids", "config_parameters", "number_of_bids_per_agent", "number_of_successful_bids_per_agent", "successful_bid_price_per_agent", "successful_opportunity_cost_per_agent", "total_bid_price_per_agent", "total_opportunity_cost_per_agent"]
 primarykeycolumns = [",cycle_number", ",cycle_number",",cycle_number", ",cycle_number,bidnumber,agentId", "", ",cycle_number",",cycle_number",",cycle_number",",cycle_number",",cycle_number",",cycle_number" ]
 def extract(logpath):
@@ -37,20 +37,32 @@ def extract(logpath):
 
 def mktables(curs, logpath):
 	for index, item in enumerate(filenames):
-        	f = gzip.open(logpath + item + ".gz")
+		name = logpath + item
+		if (name.endswith('gz')):
+			f = gzip.open(name)
+		else:
+			f = open(name)
 		row = f.readline()
 		cmd="CREATE TABLE " + tablenames[index] + " (sample,replicate," + row + ",PRIMARY KEY (sample,replicate" + primarykeycolumns[index] + "));"
+		#print(cmd)
 		curs.execute(cmd)
 
 def insert(curs, logpath, sample, replicate):
 	for index, item in enumerate(filenames):
-        	f = gzip.open(logpath + item + ".gz")
+		name = logpath + item
+		if (name.endswith('gz')):
+			f = gzip.open(name)
+		else:
+			f = open(name)
+		#print("reading " + name + "\n")
+
 		##skip the header row
 		f.readline();
 		reader = csv.reader(f)
 		for row in reader:
 			## Import row data into database
 			cmd="INSERT INTO " + tablenames[index] + " VALUES ('"  + str(sample) + "','" + str(replicate) + "','" + "','".join(row) + "')"
+			#print(cmd)
 			curs.execute(cmd)
 
 conn = sqlite3.connect(args.EXPERIMENT_DIR + '/output.db')
