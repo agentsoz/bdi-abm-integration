768789a42e8c65e174d22b151df2ce9ecc7d3a42
Dhirendra Singh <dhi.singh@gmail.com>
2018-07-16 14:50:30 +1000

    conservation: latest results

--- examples/conservation/report/plot-visits-analysis.R
+++ examples/conservation/report/plot-visits-analysis.R
@@ -11,28 +11,46 @@ csvname <- args[1]
 
 # read the csv
 csv <- read.csv(file=csvname, header=TRUE, sep=",")
-csv <- csv[c("Visit.type", "Visited.percentage", "Cost.relative.to.baseline")]
+csv <- csv[c("Visit.type", "Visits.relative.to.baseline", "Auction.cost.relative.to.baseline", "Total.cost.relative.to.baseline")]
 print(csv)
 
 plot_file_path <- sprintf('%s.pdf', csvname)
 pdf(plot_file_path)
 
-	gg <- ggplot(data=csv, aes(x=Visited.percentage, y=Cost.relative.to.baseline, group=Visit.type, shape=Visit.type, color=Visit.type)) + 
+	gg <- ggplot(data=csv, aes(x=Visits.relative.to.baseline, y=Auction.cost.relative.to.baseline, group=Visit.type, shape=Visit.type, color=Visit.type)) +
 		geom_line() +
 		geom_point(size=3) +
 		#ylim(0,100) +
 		theme_bw() +
   		theme(
 			legend.title=element_text(size=12,face="bold"),
-        	axis.title=element_text(size=12,face="bold"), 
+        	axis.title=element_text(size=12,face="bold"),
 			plot.title=element_text(size=12,face="bold",hjust=0.5),
         	aspect.ratio=5/5
 			) +
-  		xlab("Percentage of eligible landholders visited") +
-  		ylab("Cost relative to baseline") +
-		ggtitle(paste("Impact of visits in HC25 on total cost over all rounds")) +
+  		xlab("Visits relative to baseline") +
+  		ylab("Auction cost relative to baseline") +
+		ggtitle(paste("Impact of visits on HC25 population over all rounds")) +
   		guides(colour=guide_legend(title="")) +
   		guides(shape=guide_legend(title=""))
 show(gg)
-graphics.off()
 
+gg <- ggplot(data=csv, aes(x=Visits.relative.to.baseline, y=Total.cost.relative.to.baseline, group=Visit.type, shape=Visit.type, color=Visit.type)) +
+	geom_line() +
+	geom_point(size=3) +
+	#ylim(0,100) +
+	theme_bw() +
+		theme(
+		legend.title=element_text(size=12,face="bold"),
+				axis.title=element_text(size=12,face="bold"),
+		plot.title=element_text(size=12,face="bold",hjust=0.5),
+				aspect.ratio=5/5
+		) +
+		xlab("Visits relative to baseline") +
+		ylab("Total cost relative to baseline") +
+	ggtitle(paste("Impact of visits on HC25 population over all rounds")) +
+		guides(colour=guide_legend(title="")) +
+		guides(shape=guide_legend(title=""))
+show(gg)
+
+graphics.off()
