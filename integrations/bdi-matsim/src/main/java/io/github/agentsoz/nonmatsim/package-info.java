9e20d890ec2da9ceec1af45e90140e2d5fe9f29b
Dhirendra Singh <dhi.singh@gmail.com>
2021-02-19 10:23:19 +1100

    updated license headers

--- integrations/bdi-matsim/src/main/java/io/github/agentsoz/nonmatsim/package-info.java
+++ integrations/bdi-matsim/src/main/java/io/github/agentsoz/nonmatsim/package-info.java
@@ -10,7 +10,7 @@ package io.github.agentsoz.nonmatsim;
  * #%L
  * BDI-ABM Integration Package
  * %%
- * Copyright (C) 2014 - 2020 by its authors. See AUTHORS file.
+ * Copyright (C) 2014 - 2021 by its authors. See AUTHORS file.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
